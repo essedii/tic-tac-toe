b123356137d44ba0b9766a57b437c96798b2be9d
Stefano Dolci <ste.dolci16@gmail.com>
2023-11-24 09:38:31 +0100

    add combinations

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,38 @@ import { useState } from "react";
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
+import { WINNING_COMBINATIONS } from './winning-combinations';
+
+function deriveActivePlayer(gameTurns){
+  let currentPlayer = 'X';
+  if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+    currentPlayer = 'O';
+  }
+  return currentPlayer
+}
+
+let gameBoard = initialGameBoard;
+for (const turn of turns) {
+    const {square, player} = turn;
+    const {row, col} = square;
+
+    gameBoard[row][col] = player;
+}
+
+for (combination of WINNING_COMBINATIONS) {
+  const firstSquare = [];
+  const secondSquare = [];
+  const thirdSquare = [];
+}
 
 function App() {
-  const [gameTurns, setGameTurns] = useState([]); 
-  const [activePlayer, setActivePlayer] = useState('X');
+  const [gameTurns, setGameTurns] = useState([]);
+
+  const activePlayer = deriveActivePlayer(gameTurns);
 
   function handleSelectSquare(rowIndex, colIndex) {
-    setActivePlayer((currActivePlayer)=> currActivePlayer==='X'? 'O': 'X');
-    setGameTurns(prevTurns=> {
-      let currentPlayer = 'X';
-
-      if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
-        currentPlayer = 'O';
-      }
-
+    setGameTurns((prevTurns) => {
+      const currentPlayer = deriveActivePlayer(prevTurns);
       const updateTurn = [{ square:{row: rowIndex, col: colIndex}, player: currentPlayer},...prevTurns];
       return updateTurn;
     })
@@ -24,12 +42,12 @@ function App() {
     <main>
         <div id="game-container">
           <ol id="players" className="highlight-player">
-            <Player initialName="Player 1" symbol="X" isActive={activePlayer==='X'}/>
-            <Player initialName="Player 2" symbol="O" isActive={activePlayer==='O'}/>
+            <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
+            <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}/>
           </ol>
-          <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns}/>
+          <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard}/>
         </div>
-        <Log/>
+        <Log turns={gameTurns} />
     </main>
   )
 }
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,43 +1,24 @@
-// import { useState } from "react";
-
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
     [null, null, null],
   ];
   
-export default function GameBoard({ onSelectSquare, turns }) {
-    let gameBoard = initialGameBoard;
-    for (const turn of turns) {
-        const {square, player} = turn;
-        const {row, col} = square;
+export default function GameBoard({ onSelectSquare, gameBoard }) {
 
-        gameBoard[row][col] = player;
-    }
-    // const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-    // function handleSelectSquare(rowIndex, colIndex) {
-    //     setGameBoard((previousGameBoard)=> {
-    //         const updatedBoard = [...previousGameBoard.map(innerArray => [...innerArray])];
-    //         updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-    //         return  updatedBoard
-    //     });
-
-    //     onSelectSquare();
-    // }
   return (
     <ol id="game-board">
-        {gameBoard.map((row, rowIndex)=>(
-   <li key={rowIndex}>
-   <ol>
-     {row.map((playerSymbol, colIndex) => (
-       <li key={colIndex}>
-         <button onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
-       </li>
-     ))}
-   </ol>
- </li>
-        ))}
+      {gameBoard.map((row, rowIndex)=>(
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button onClick={() => onSelectSquare(rowIndex, colIndex)} disabled={playerSymbol !== null}>{playerSymbol}</button>
+              </li>          
+            ))}
+          </ol>
+        </li>
+      ))}
     </ol>
   );
 }
